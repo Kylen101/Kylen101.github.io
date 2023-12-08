eccfe251284214db7977545e87a5fcc1dced0d8d
Kylen101 <152532364+Kylen101@users.noreply.github.com>
2023-12-08 11:11:47 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ for (let i = 0; i < 99999; i++) {
   numbers.push(i);
 }
 console.log(numbers.length)
+
+let = isGreen = true
+setInterval(function () {
+  if (isGreen){
+    isGreen = false
+      document.getElementById("vote").style.color = "#fa0202";  
+  } else {
+    isGreen = true
+    document.getElementById("vote").style.color = "black";      
+  }
+
+}, 1500);
