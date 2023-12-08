6c504a5c4677dd62421056ea1b8eb1cdc717980f
Virtualfax <88285925+Virtualfax@users.noreply.github.com>
2023-12-08 11:04:16 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-function promptFunction() {
-  let namePrompt = prompt("Please enter your name", "Lazy user");
-  if (namePrompt != null) {
-    document.getElementById("nameDisplay").innerHTML =
-    "Hello " + namePrompt + "! I am going to beat you up!";
-  }
-}
-let var1 = 20
-console.log(var1)
+
